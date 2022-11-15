99e9e7e2541f224e49efe92cfd5d69dc116242c9
davidsvaughn <davidsvaughn@gmail.com>
2022-11-15 04:09:52 -0500

    float to int cast (https://github.com/iscyy/yoloair/pull/48)

--- utils/loss.py
+++ utils/loss.py
@@ -264,7 +264,14 @@ class ComputeLoss:
             # Append
             a = t[:, -1].long()  # anchor indices 取整
             # indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
-            indices.append((b, a, gj.clamp_(0, feature_wh[1] - 1), gi.clamp_(0, feature_wh[0] - 1)))  # image, anchor, grid indices
+
+            ####
+            # indices.append((b, a, gj.clamp_(0, feature_wh[1] - 1), gi.clamp_(0, feature_wh[0] - 1)))  # image, anchor, grid indices
+            val1 = gj.clamp_(0, int(feature_wh[1] - 1))
+            val2 = gi.clamp_(0, int(feature_wh[0] - 1))
+            indices.append((b, a, val1, val2))  # image, anchor, grid indices
+            ####
+
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
